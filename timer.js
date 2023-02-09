c758fc9580ef3d7808380725e43e26a6a236ff04
Magda Bieńkowska <m.lena.bienkowska@gmail.com>
2023-02-09 15:59:09 +0100

    Add new files

---
+++ timer.js
@@ -0,0 +1,52 @@
+class Timer {
+    constructor(durationInput, startBtn, pauseBtn, callbacks) {
+        this.durationInput = durationInput;
+        this.startBtn = startBtn;
+        this.pauseBtn = pauseBtn;
+
+        if(callbacks) {
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
+
+        startBtn.addEventListener("click", this.start)
+        pauseBtn.addEventListener("click", this.pause)
+    }
+
+    start = () => {
+        if(this.onStart) {
+            this.onStart(this.timeRemaining)
+        }
+        this.tick()
+        this.interval = setInterval(this.tick, 20);
+    }
+
+    pause = () => {
+        clearInterval(this.interval)
+    }
+
+    // ten pierwszy timeRemaining to ten z set, ten drugi to z get
+
+    tick = () => {
+        if(this.timeRemaining <= 0) {
+            this.pause()
+            if(this.onComplete) {
+                this.onComplete()
+            }
+        } else {
+            this.timeRemaining = this.timeRemaining - 0.02;
+            if(this.onTick) {
+                this.onTick(this.timeRemaining)
+            }
+        }
+    }
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value)
+    }
+
+    set timeRemaining(time) {
+        this.durationInput.value = time.toFixed(2);
+    }
+}
